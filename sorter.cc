ce66c9cd2fda9ad4cb7f20f67e5e98e348857d58
benrafalski <79751177+benrafalski@users.noreply.github.com>
2022-07-05 09:42:16 -0700

    bubble sort c++

--- sorter.cc
+++ sorter.cc
@@ -16,6 +16,8 @@ void quick_sort(int arr[], int low, int high);
 void merge(int arr[], int const left, int const mid, int const right);
 void merge_sort(int arr[], int const begin, int const end);
 
+void bubble_sort(int arr[], int n);
+
 int *random_arr()
 {
     static int a[SIZE];
@@ -151,13 +153,30 @@ void merge_sort(int arr[], int const begin, int const end)
     merge(arr, begin, mid, end);
 }
 
+void bubble_sort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int t = *&arr[j];
+                *&arr[j] = *&arr[j + 1];
+                *&arr[j + 1] = t;
+            }
+        }
+    }
+}
+
 int main()
 {
     int *arr = random_arr();
     print(arr);
     // insertion_sort(arr, SIZE);
     // quick_sort(arr, 0, SIZE - 1);
-    merge_sort(arr, 0, SIZE - 1);
+    // merge_sort(arr, 0, SIZE - 1);
+    bubble_sort(arr, SIZE);
     print(arr);
 
     return 0;
