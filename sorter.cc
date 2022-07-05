de4d8796b6d0d1d33c7ec7bc90de8f6557a1db17
benrafalski <79751177+benrafalski@users.noreply.github.com>
2022-07-04 17:54:03 -0700

    quicksort done c++

--- sorter.cc
+++ sorter.cc
@@ -8,7 +8,8 @@ using namespace std;
 int *random_arr();
 void print(int arr[]);
 void insertion_sort(int arr[], int n);
-int partition(int arr[], int low, int high)
+int partition(int arr[], int low, int high);
+void quick_sort(int arr[], int low, int high);
 
 int *random_arr()
 {
@@ -24,14 +25,17 @@ int *random_arr()
     return a;
 }
 
-void print(int arr[]){
+void print(int arr[])
+{
     cout << "[";
-    for(int i = 0; i < SIZE; i++){
-        if(i == SIZE -1){
+    for (int i = 0; i < SIZE; i++)
+    {
+        if (i == SIZE - 1)
+        {
             cout << arr[i] << "]\n";
             return;
         }
-        cout << arr[i] << ", "; 
+        cout << arr[i] << ", ";
     }
 }
 
@@ -52,17 +56,44 @@ void insertion_sort(int arr[], int n)
     }
 }
 
+int partition(int arr[], int low, int high)
+{
+    int pivot = arr[high];
+    int i = (low - 1);
+    for (int j = low; j <= high - 1; j++)
+    {
+        if (arr[j] < pivot)
+        {
+            i++;
+            int t = *&arr[i];
+            *&arr[i] = *&arr[j];
+            *&arr[j] = t;
+        }
+    }
+    int t = *&arr[i + 1];
+    *&arr[i + 1] = *&arr[high];
+    *&arr[high] = t;
+    return (i + 1);
+}
 
+void quick_sort(int arr[], int low, int high)
+{
+    if (low < high)
+    {
+        int pi = partition(arr, low, high);
 
+        quick_sort(arr, low, pi - 1);
+        quick_sort(arr, pi + 1, high);
+    }
+}
 
 int main()
 {
-    int * arr = random_arr();
+    int *arr = random_arr();
     print(arr);
-    insertion_sort(arr, SIZE);
+    // insertion_sort(arr, SIZE);
+    quick_sort(arr, 0, SIZE - 1);
     print(arr);
-
-
 
     return 0;
 }
