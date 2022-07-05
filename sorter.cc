61e546ab5c341d3a828cdd104bc8275951d28c45
benrafalski <79751177+benrafalski@users.noreply.github.com>
2022-07-04 17:29:07 -0700

    insertion sort and random numbers c++

--- sorter.cc
+++ sorter.cc
@@ -1,10 +1,41 @@
 #include <iostream>
+#include <ctime>
 
+using namespace std;
 
-int main(){
+int *random_arr();
+void insertion_sort(int arr[], int n);
 
+int *random_arr()
+{
+    static int a[20];
+    srand((unsigned)time(NULL));
+    for (int i = 0; i < 10; ++i)
+    {
+        a[i] = rand();
+    }
+    return a;
+}
 
+void insertion_sort(int arr[], int n)
+{
+    int i, key, j;
+    for (i = 1; i < n; i++)
+    {
+        key = arr[i];
+        j = i - 1;
 
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+int main()
+{
 
     return 0;
 }
