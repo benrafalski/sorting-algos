9d7c6d5c7dd535d00620278851564ddd16ba1d85
benrafalski <79751177+benrafalski@users.noreply.github.com>
2022-07-04 17:42:20 -0700

    insertion + helpers done

--- sorter.cc
+++ sorter.cc
@@ -3,18 +3,36 @@
 
 using namespace std;
 
+#define SIZE 10
+
 int *random_arr();
+void print(int arr[]);
 void insertion_sort(int arr[], int n);
+int partition(int arr[], int low, int high)
 
 int *random_arr()
 {
-    static int a[20];
+    static int a[SIZE];
+    int max = 100, min = 0;
+    int range = max - min + 1;
+
     srand((unsigned)time(NULL));
-    for (int i = 0; i < 10; ++i)
+    for (int i = 0; i < SIZE; ++i)
     {
-        a[i] = rand();
+        a[i] = rand() % range + min;
     }
     return a;
+}
+
+void print(int arr[]){
+    cout << "[";
+    for(int i = 0; i < SIZE; i++){
+        if(i == SIZE -1){
+            cout << arr[i] << "]\n";
+            return;
+        }
+        cout << arr[i] << ", "; 
+    }
 }
 
 void insertion_sort(int arr[], int n)
@@ -34,8 +52,17 @@ void insertion_sort(int arr[], int n)
     }
 }
 
+
+
+
 int main()
 {
+    int * arr = random_arr();
+    print(arr);
+    insertion_sort(arr, SIZE);
+    print(arr);
+
+
 
     return 0;
 }
