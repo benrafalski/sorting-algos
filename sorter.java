e2969d00af28ff5c02a8dc0f679f8b310f8de94f
benrafalski <79751177+benrafalski@users.noreply.github.com>
2022-07-06 18:19:47 -0700

    name change

--- sorter.java
+++ sorter.java
@@ -8,7 +8,32 @@ public class Sorter {
         this.size = arr.length;
     }
 
-    
+    public void insertion_sort()
+    {
+        int i, key, j;
+        for (i = 1; i < size; i++)
+        {
+            key = array[i];
+            j = i - 1;
+
+            while (j >= 0 && array[j] > key)
+            {
+                array[j + 1] = array[j];
+                j = j - 1;
+            }
+            array[j + 1] = key;
+        }
+    }
+
+    public void partition(){
+
+
+    }
+
+    public void quick_sort(){
+
+        
+    }
 
 
 
