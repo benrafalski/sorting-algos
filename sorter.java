88babd17cdcc0b3fed8809136ef235cc10b421be
benrafalski <79751177+benrafalski@users.noreply.github.com>
2022-07-06 18:22:34 -0700

    added quicksort java

--- sorter.java
+++ sorter.java
@@ -25,14 +25,33 @@ public class Sorter {
         }
     }
 
-    public void partition(){
-
+    public int partition(int high, int low){
+        int pivot = array[high];
+        int i = (low - 1);
+        for (int j = low; j <= high - 1; j++)
+        {
+            if (array[j] < pivot)
+            {
+                i++;
+                int t = array[i];
+                array[i] = array[j];
+                array[j] = t;
+            }
+        }
+        int t = array[i + 1];
+        array[i + 1] = array[high];
+        array[high] = t;
+        return (i + 1);
 
     }
 
-    public void quick_sort(){
-
-        
+    public void quick_sort(int low, int high){
+        if (low < high)
+        {
+            int pi = partition(low, high);
+            quick_sort(low, pi - 1);
+            quick_sort(pi + 1, high);
+        }
     }
 
 
