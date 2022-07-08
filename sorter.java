5cf519a52246985f5a70d49dacc995bb775a2d14
benrafalski <79751177+benrafalski@users.noreply.github.com>
2022-07-08 15:12:13 -0700

    moved everything into the sorter file, removed the driver file

--- sorter.java
+++ sorter.java
@@ -1,61 +1,51 @@
 public class Sorter {
 
-    int[] array;
-    int size;
+  public static void main(String[] args) {
+    int[] arr = { 4, 6, 3, 65, 10 };
+    // insertion_sort(arr);
+    quick_sort(arr, 0, 4);
 
-    public Sorter(int[] arr){
-        this.array = arr;
-        this.size = arr.length;
+    for (int i = 0; i < 5; i++) {
+      System.out.println(arr[i]);
     }
+  }
 
-    public void insertion_sort()
-    {
-        int i, key, j;
-        for (i = 1; i < size; i++)
-        {
-            key = array[i];
-            j = i - 1;
+  public static void insertion_sort(int[] array) {
+    int i, key, j;
+    for (i = 1; i < array.length; i++) {
+      key = array[i];
+      j = i - 1;
 
-            while (j >= 0 && array[j] > key)
-            {
-                array[j + 1] = array[j];
-                j = j - 1;
-            }
-            array[j + 1] = key;
-        }
+      while (j >= 0 && array[j] > key) {
+        array[j + 1] = array[j];
+        j = j - 1;
+      }
+      array[j + 1] = key;
     }
+  }
 
-    static public int partition(int[] a, int high, int low){
-        int pivot = a[high];
-        int i = (low - 1);
-        for (int j = low; j <= high - 1; j++)
-        {
-            if (a[j] < pivot)
-            {
-                i++;
-                int t = a[i];
-                a[i] = a[j];
-                a[j] = t;
-            }
-        }
-        int t = a[i + 1];
-        a[i + 1] = a[high];
-        a[high] = t;
-        return (i + 1);
-
+  public static int partition(int[] a, int low, int high) {
+    int pivot = a[high];
+    int i = (low - 1);
+    for (int j = low; j <= high - 1; j++) {
+      if (a[j] < pivot) {
+        i++;
+        int t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+      }
     }
+    int t = a[i + 1];
+    a[i + 1] = a[high];
+    a[high] = t;
+    return (i + 1);
+  }
 
-    static public void quick_sort(int[] a, int low, int high){
-        System.out.print("here\n");
-        int[] arr = a;
-        if (low < high)
-        {
-            int pi = partition(arr, low, high);
-            quick_sort(arr, low, pi - 1);
-            quick_sort(arr, pi + 1, high);
-        }
+  public static void quick_sort(int[] a, int low, int high) {
+    if (low < high) {
+      int pi = partition(a, low, high);
+      quick_sort(a, low, pi - 1);
+      quick_sort(a, pi + 1, high);
     }
-
-
-
+  }
 }
