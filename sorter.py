05575eaa1b31c4ab68c4be7242dd9ce4679ce303
benrafalski <79751177+benrafalski@users.noreply.github.com>
2022-07-09 07:45:33 -0700

    added quicksort python

--- sorter.py
+++ sorter.py
@@ -1,4 +1,26 @@
 
+def partition(arr, low, high):
+    pivot = arr[high]
+    i = (low - 1)
+    for j in range(low, high):
+        if arr[j] < pivot:
+            i = i + 1
+            t = arr[i]
+            arr[i] = arr[j]
+            arr[j] = t
+    t = arr[i + 1]
+    arr[i + 1] = arr[high]
+    arr[high] = t
+    return (i + 1)
+
+
+def quick_sort(arr, low, high):
+    if low < high:
+        pi = partition(arr, low, high)
+        quick_sort(arr, low, pi - 1)
+        quick_sort(arr, pi + 1, high)
+    
+
 
 def insertion_sort(arr, n):
     # int i, key, j;
@@ -13,7 +35,8 @@ def insertion_sort(arr, n):
 
 def main():
     arr = [5,3,4,1,8]
-    insertion_sort(arr, 5)
+    # insertion_sort(arr, 5)
+    quick_sort(arr, 0, 4)
     for i in range(5):
         print(arr[i])
 
